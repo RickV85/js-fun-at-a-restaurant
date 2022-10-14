307a7670b0baca574fad99a3eeaa68bcae611a0c
Rick Vermeil <rickv85@gmail.com>
2022-10-13 18:26:28 -0600

    Update order

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,51 @@
-function nameMenuItem() {
+// name menu
+function nameMenuItem(food) {
+  return (`Delicious ${food}`)
+};
 
+// create menu item
+function createMenuItem(name, price, type) {
+  var menuItem = ({
+    name: name,
+    price: price,
+    type: type
+  });
+  return menuItem
+};
+
+// add ingredients
+function addIngredients(ingredient, ingredients) {
+  if (ingredients.includes(ingredient) === true) {
+    return
+  } else {
+    ingredients.push(ingredient);
+  }
+};
+
+// format price
+function formatPrice(price) {
+  return `$${price}`;
+};
+
+// decrease price
+function decreasePrice(price) {
+  return price * .9
+};
+
+// create recipe
+function createRecipe(name, ing, type) {
+  return {
+  title: name,
+  ingredients: ing,
+  type: type,
+  }
 }
-
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
