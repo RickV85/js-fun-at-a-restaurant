0162a593904d2defd7ddce15f0f3926cb8af52ee
Rick Vermeil <rickv85@gmail.com>
2022-10-15 18:35:53 -0600

    All tests passing

--- src/order.js
+++ src/order.js
@@ -1,7 +1,44 @@
+// take order
+function takeOrder(newOrder, allOrders) {
+  if (allOrders.length > 2) {
+    return allOrders;
+  } else {
+  allOrders.push(newOrder)
+  }
+};
+
+// refund order
+function refundOrder(order_num, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === order_num) {
+      return deliveryOrders.splice(i, 1)
+    }
+  }
+};
+
+// list items
+function listItems(deliveryOrders) {
+  var items = []
+  for (let i = 0; i < deliveryOrders.length; i++) {
+     items.push(deliveryOrders[i].item);
+  }
+  return items.join(', ');
+};
+
+// search order
+function searchOrder(orders, food) {
+  var match = false
+  for (let i = 0; i < orders.length; i++) {
+    if (orders[i].item === food) {
+      match = true;
+    }
+  }
+  return match;
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,51 @@
+// create createRestaurant
+function createRestaurant(name) {
+  var pizzaRestaurant = {
+    name: name,
+    menus: {breakfast: [], lunch: [], dinner: []}
+  }
+  return pizzaRestaurant;
+}
 
+// addMenuItem
+function addMenuItem(restMenu, newItem) {
+  if (newItem.type === "breakfast" && !restMenu.menus.breakfast.includes(newItem)) {
+    restMenu.menus.breakfast.push(newItem);
+  } else if (newItem.type === "lunch" && !restMenu.menus.lunch.includes(newItem)) {
+    restMenu.menus.lunch.push(newItem);
+  } else if (newItem.type === "dinner" && !restMenu.menus.dinner.includes(newItem)) {
+    restMenu.menus.dinner.push(newItem);
+  }
+}
+
+// removeMenuItem
+// restaurant, itemName, menu
+function removeMenuItem(restMenu, removeItem, itemMenu) {
+  if (restMenu.menus.breakfast.length < 1) {
+    return `Sorry, we don't sell ${removeItem}, try adding a new recipe!`;
+  }
+  if (restMenu.menus.lunch.length < 1) {
+    return `Sorry, we don't sell ${removeItem}, try adding a new recipe!`;
+  }
+  if (restMenu.menus.dinner.length < 1) {
+    return `Sorry, we don't sell ${removeItem}, try adding a new recipe!`;
+  }
+  for (var i = 0; i < restMenu.menus.breakfast.length; i++) {
+    if (restMenu.menus.breakfast[i].name === removeItem) {
+      restMenu.menus.breakfast.pop();
+      return `No one is eating our ${removeItem} - it has been removed from the breakfast menu!`;
+    }
+  }
+  for (var i = 0; i < restMenu.menus.dinner.length; i++) {
+    if (restMenu.menus.dinner[i].name === removeItem) {
+        restMenu.menus.dinner.pop();
+      return `No one is eating our ${removeItem} - it has been removed from the dinner menu!`;
+    }
+  }
+};
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
