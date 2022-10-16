2a9f6b230eae8e7ed20e10311e74e61edabb102c
Rick Vermeil <rickv85@gmail.com>
2022-10-16 09:55:19 -0600

    Add Chef

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,20 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+  greetCustomer(customer, morning){
+    if (morning) {
+      return `Good morning, ${customer}!`
+    } else
+    return (`Hello, ${customer}!`)
+  }
+  checkForFood(checkFood) {
+    if (this.restaurant.menus.breakfast.includes(checkFood)) {
+      return `Yes, we're serving ${checkFood.name} today!`
+    } else {
+      return `Sorry, we aren't serving ${checkFood.name} today.`
+    }
   }
 }
 
